924fd8c8a8bfb2db1b47021053212b79469bc45c
Donnie D'Amato <fauxserious@gmail.com>
2021-07-08 15:45:22 -0400

    Comments

--- src/jfauna.js
+++ src/jfauna.js
@@ -1,16 +1,32 @@
 const m = require('./methods');
 
+/**
+ * Creates a new instance of jFauna
+ * 
+ * @param {FaunaDBClient} client - A FaunaDB client prepared to make requests on behalf of the server
+ * @returns {Object} - New instance of jFauna
+ */
 function jFauna(client) {
 
+  // Storing the client on the instance
   this._client = client;
+
+  // Turns on caching for collections/indexes
+  this.cache = true;
+
+  // Returns the complete response from the DB, when false we return just the data field.
+  this.raw = false;
+
+  // Allows the promise for ensuring the collection exists to resolve later
   this._promises = new Set();
+
+  // The cache for collections/indexes so we don't need to ensure they exist each time
   this._collectionCache = new Set();
   this._indexCache = new Set();
-  this.cache = true;
-  this.raw = false;
 
   const instance = (collectionName) => {
     
+    // All of the primary methods from the collection
     const methods = {
       resolve: m.resolve.bind(this),
       insert: m.insert.bind(this),
@@ -19,11 +35,21 @@ function jFauna(client) {
       update: m.update.bind(this),
     };
 
+    // Storing the collection name to be referenced while preparing queries
     this._currentCollectionName = collectionName;
+
+    // Ensure the collection exists before proceeding, return the methods when resolved.
     const initCollection = m.init.call(this, methods);
+
+    // Allows this to be awaited later in the chain
     this._promises.add(initCollection);
+
+    // Return the promise overloaded with the methods. The promise resolves to the methods also.
     return Object.assign(initCollection, methods);
+
   };
+
+  // Allow an instance to be created
   return Object.setPrototypeOf(instance, jFauna.prototype);
 }
 
--- src/methods.js
+++ src/methods.js
@@ -3,48 +3,68 @@ const { collection, document, index } = require('./queries/queries');
 const FAUNA_PAGINATION_SIZE = 64;
 
 const methods = {
+
+  // Ensures the collection exists, returns the bound methods for the jFauna instance
   init: async function (m) {
     await collection.ensure.call(this);
     return m;
   },
 
+  // Resolves the group of promises before proceeding
   resolve: async function () {
     await Promise.all(Array.from(this._promises));
     this._promises.clear();
   },
 
+  // Inserts record(s) in the current collection
   insert: async function (data) {
     await methods.resolve.call(this);
     await document.create.call(this, data);
   },
 
+  // Gets record(s) in the current collection
   get: function (size = FAUNA_PAGINATION_SIZE) {
     return chain.call(this, document.get, { size });
   },
 
+  // Deletes record(s) in the current collection
   remove: function (size = FAUNA_PAGINATION_SIZE) {
     return chain.call(this, document.remove, { size });
   },
 
+  // Updates record(s) in the current collection
   update: function(data) {
     return chain.call(this, document.update, { data });
   }
 };
 
+/**
+ * Creates the chainable method
+ * 
+ * @param {Function} operation - The function to run based on the method selected
+ * @param {Object} params - Additional parameters to inform later functions
+ * @param {Number} [params.size] - The amount of records to affect with the query
+ * @param {Object} [params.data] - Data to update each of the records
+ * @returns {Object} - Chainable methods
+ */
 function chain(operation, params) {
   return {
+
     now: async () => {
       await methods.resolve.call(this);
       const ref = await index.name.call(this, 'all');
       return await operation.call(this, { ...params, index: ref })
     },
+
     where: (field) => {
       return {
+
         is: async (value) => {
           await methods.resolve.call(this);
           const ref = await index.name.call(this, 'equals', field);
           return await operation.call(this, { ...params, index: ref, field, value })
         },
+        
         isnt: async (value) => {
           await methods.resolve.call(this);
           const [ref, compare] = await Promise.all([
